18e47da1e51874d9a9a5a4855667e6f789f94f6a
Caleb Van Lue <cpvanlue@bsu.edu>
2020-09-23 23:15:20 -0400

    Wikipedia connection demo and tests added

---
+++ src/main/java/edu/bsu/cs222/WikipediaConnectionDemo.java
@@ -0,0 +1,58 @@
+package edu.bsu.cs222;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class WikipediaConnectionDemo {
+
+    public static void main(String[] args)
+            throws IOException, ParserConfigurationException, SAXException, TransformerException {
+        URLConnection connection = connectToWikipedia();
+        Document document = readXmlDocumentFrom(connection);
+        printDocument(document);
+    }
+
+    private static URLConnection connectToWikipedia() throws IOException {
+        URL url = new URL(
+                "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=xml&rvprop=timestamp%7Cuser&rvlimit=4&titles=frank%20zappa&redirects=");
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent",
+                "CS222FirstProject/0.1 (dllargent@bsu.edu)");
+        connection.connect();
+        return connection;
+    }
+
+    private static Document readXmlDocumentFrom(URLConnection connection) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        return documentBuilder.parse(connection.getInputStream());
+    }
+
+    private static void printDocument(Document doc) throws TransformerException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+        transformer.transform(new DOMSource(doc), new StreamResult(new OutputStreamWriter(System.out, StandardCharsets.UTF_8)));
+    }
+
+}
