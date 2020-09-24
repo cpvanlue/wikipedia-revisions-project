dd78a1ba5b04ea23a9e443897c6b7355bb2f8308
Caleb Van Lue <cpvanlue@bsu.edu>
2020-09-24 14:00:31 -0400

    WikipediaConnection switched to return Json data string.

--- src/main/java/edu/bsu/cs222/SearchTermToUrl.java
+++
@@ -1,9 +0,0 @@
-package edu.bsu.cs222;
-
-public class SearchTermToUrl {
-
-    public String convert(String input) {
-        String inputReplaceSpace = input.replaceAll(" ", "+");
-        return inputReplaceSpace;
-    }
-}
--- src/main/java/edu/bsu/cs222/WikipediaConnection.java
+++ src/main/java/edu/bsu/cs222/WikipediaConnection.java
@@ -1,38 +1,23 @@
 package edu.bsu.cs222;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 public class WikipediaConnection {
 
-//    public static void main(String[] args)
-//            throws IOException, ParserConfigurationException, SAXException, TransformerException {
-//        URLConnection connection = connectToWikipedia();
-//        Document document = readXmlDocumentFrom(connection);
-//        printDocument(document);
-//    }
+    public static void main(String[] args) throws IOException {
+        URLConnection connection = connectToWikipedia();
+        String jsonData = readJsonDataFrom(connection);
+        System.out.println(jsonData);
+    }
 
-    private static URLConnection connectToWikipedia(String input) throws IOException {
-        SearchTermToUrl searchTerm = new SearchTermToUrl();
-        searchTerm.convert(input);
+    private static URLConnection connectToWikipedia() throws IOException {
         URL url = new URL(
-                "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=xml&rvprop=timestamp%7Cuser&rvlimit=20&titles=" + searchTerm + "&redirects=");
+                "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=timestamp%7Cuser&rvlimit=20&titles=zappa&redirects=");
         URLConnection connection = url.openConnection();
         connection.setRequestProperty("User-Agent",
                 "CS222FirstProject/0.1 (cpvanlue@bsu.edu)");
@@ -40,21 +25,13 @@ public class WikipediaConnection {
         return connection;
     }
 
-    private static Document readXmlDocumentFrom(URLConnection connection) throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        return documentBuilder.parse(connection.getInputStream());
+    private static String readJsonDataFrom(URLConnection connection) throws IOException {
+        StringBuilder jsonStringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String jsonData;
+        while ((jsonData = bufferedReader.readLine()) != null) {
+            jsonStringBuilder.append(jsonData + "\n");
+        }
+        return jsonStringBuilder.toString().trim();
     }
-
-    private static void printDocument(Document doc) throws TransformerException {
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = tf.newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-        transformer.transform(new DOMSource(doc), new StreamResult(new OutputStreamWriter(System.out, StandardCharsets.UTF_8)));
-    }
-
 }
