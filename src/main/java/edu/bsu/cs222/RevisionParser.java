6e60344aa73bcc0e7fe1e46f96debc21eeaf68cf
Hailey Leonard <heleonard@bsu.edu>
2020-09-27 16:43:26 -0400

    Edited Parse method to return a list of all revisions.

--- src/main/java/edu/bsu/cs222/RevisionParser.java
+++ src/main/java/edu/bsu/cs222/RevisionParser.java
@@ -4,19 +4,22 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class RevisionParser {
-    public String parse(JsonObject object){
+    public List<JsonObject> parse(JsonObject object){
         JsonObject pages = object.getAsJsonObject("query").getAsJsonObject("pages");
         JsonArray array = null;
+        List<JsonObject> revisionsList = new ArrayList<>();
         for (Map.Entry<String, JsonElement> entry : pages.entrySet()) {
             JsonObject entryObject = entry.getValue().getAsJsonObject();
             array = entryObject.getAsJsonArray("revisions");
         }
-        JsonElement firstRevision = array.get(0);
-        JsonObject firstObject = firstRevision.getAsJsonObject();
-        String name = firstObject.get("user").toString();
-        return name;
+        for (int i=0; i<array.size(); i++){
+            revisionsList.add(array.get(i).getAsJsonObject());
+        }
+        return revisionsList;
     }
 }
