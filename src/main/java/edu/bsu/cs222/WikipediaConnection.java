20fca1a7981b03918455bedce786bd4c1d9fc45a
Hailey Leonard <heleonard@bsu.edu>
2020-09-27 15:44:25 -0400

    Edited WikipediaConnection class to include keywordToURL method and return JSONObject

--- src/main/java/edu/bsu/cs222/WikipediaConnection.java
+++ src/main/java/edu/bsu/cs222/WikipediaConnection.java
@@ -1,5 +1,8 @@
 package edu.bsu.cs222;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,15 +12,9 @@ import java.net.URLConnection;
 
 public class WikipediaConnection {
 
-    public static void main(String[] args) throws IOException {
-        URLConnection connection = connectToWikipedia();
-        String jsonData = readJsonDataFrom(connection);
-        System.out.println(jsonData);
-    }
-
-    public static URLConnection connectToWikipedia() throws IOException {
+    public static URLConnection connectToWikipedia(String searchTerm) throws IOException {
         URL url = new URL(
-                "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=timestamp%7Cuser&rvlimit=20&titles=zappa&redirects=");
+                "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=timestamp%7Cuser&rvlimit=20&titles="+searchTerm+"&redirects=");
         URLConnection connection = url.openConnection();
         connection.setRequestProperty("User-Agent",
                 "CS222FirstProject/0.1 (cpvanlue@bsu.edu)");
@@ -25,13 +22,20 @@ public class WikipediaConnection {
         return connection;
     }
 
-    public static String readJsonDataFrom(URLConnection connection) throws IOException {
+    public static JsonObject readJsonDataFrom(URLConnection connection) throws IOException {
         StringBuilder jsonStringBuilder = new StringBuilder();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String jsonData;
         while ((jsonData = bufferedReader.readLine()) != null) {
             jsonStringBuilder.append(jsonData + "\n");
         }
-        return jsonStringBuilder.toString().trim();
+        String websiteInfo = jsonStringBuilder.toString().trim();
+        JsonParser jsonParser = new JsonParser();
+        JsonObject object = (JsonObject) jsonParser.parse(websiteInfo);
+        return object;
+    }
+
+    public static String keywordToURL (String input){
+        return input.replaceAll(" ", "+");
     }
 }
