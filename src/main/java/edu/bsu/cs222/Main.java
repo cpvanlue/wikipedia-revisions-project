555e2c0da2b0a5a65d45dfde9df95e1045f522c8
Hailey Leonard <heleonard@bsu.edu>
2020-10-06 00:27:37 -0400

    Finished GUI design. Started identifying variables.

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -1,12 +1,29 @@
 package edu.bsu.cs222;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import static edu.bsu.cs222.UI.searchAndPullWikiData;
 
-public class Main {
+public class Main extends Application {
 
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        String fxmlResource = "GUI.fxml";
+        Parent panel;
+        panel = FXMLLoader.load(getClass().getClassLoader().getResource(fxmlResource));
+        Scene scene = new Scene(panel);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+    }
     public static void main(String[] args) {
-
-        searchAndPullWikiData();
+        launch(args);
 
     }
 }
